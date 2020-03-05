71f2e5abf6258086f1078bd6ad64789cd8fe972f
Nazariy995 <nazariy1995@gmail.com>
2020-03-04 18:19:10 -0800

    ADD: functions to the exercise

---
+++ exercise/js/index.js
@@ -0,0 +1,47 @@
+const URL = "http://localhost:3000/tweets";
+
+/**
+ * Retrive Twitter Data from API
+ */
+const getTwitterData = () => {
+
+}
+
+/**
+ * Save the next page data
+ */
+const saveNextPage = (metadata) => {
+}
+
+/**
+ * Handle when a user clicks on a trend
+ */
+const selectTrend = (e) => {
+}
+
+/**
+ * Set the visibility of next page based on if there is data on next page
+ */
+const nextPageButtonVisibility = (metadata) => {
+}
+
+/**
+ * Build Tweets HTML based on Data from API
+ */
+const buildTweets = (tweets, nextPage) => {
+
+}
+
+/**
+ * Build HTML for Tweets Images
+ */
+const buildImages = (mediaList) => {
+
+}
+
+/**
+ * Build HTML for Tweets Video
+ */
+const buildVideo = (mediaList) => {
+
+}
